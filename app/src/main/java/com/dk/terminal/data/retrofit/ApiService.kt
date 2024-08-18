c851c4dcdf5d17390c9b99f91aafa5e26b0d2bdf
dk <kdi_2004@mail.ru>
2024-08-19 00:25:05 +0300

    изучили derivedStateOf

--- app/src/main/java/com/dk/terminal/data/retrofit/ApiService.kt
+++ app/src/main/java/com/dk/terminal/data/retrofit/ApiService.kt
@@ -4,6 +4,6 @@ import com.dk.terminal.data.model.ResultDto
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("aggs/ticker/X:BTCUSD/range/1/day/2023-01-09/2024`-02-10?adjusted=true&sort=desc&apiKey=zkmRNsxNlA99WQBKF2k6ICtGFri2oJ6J")
+    @GET("aggs/ticker/X:BTCUSD/range/1/day/2023-01-09/2024-02-10?adjusted=true&sort=desc&apiKey=zkmRNsxNlA99WQBKF2k6ICtGFri2oJ6J")
     suspend fun loadBars(): ResultDto
 }
