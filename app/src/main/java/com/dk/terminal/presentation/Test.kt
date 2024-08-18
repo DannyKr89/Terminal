352c0308137881082cddc9b939bcb8f88ef50587
dk <kdi_2004@mail.ru>
2024-08-19 00:39:26 +0300

    сохраняем стейт с помощью Saver объекта с одним полем

--- app/src/main/java/com/dk/terminal/presentation/Test.kt
+++ app/src/main/java/com/dk/terminal/presentation/Test.kt
@@ -1,23 +1,27 @@
 package com.dk.terminal.presentation
 
-import android.os.Parcelable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.parcelize.Parcelize
 
 @Composable
 fun Test(modifier: Modifier = Modifier) {
-    var testData by rememberSaveable {
+
+    var testData by rememberSaveable(
+        saver = TestData.saver
+    ) {
         mutableStateOf(TestData(0))
+
     }
     Box(
         modifier = modifier
@@ -29,5 +33,15 @@ fun Test(modifier: Modifier = Modifier) {
     }
 }
 
-@Parcelize
-data class TestData(val number: Int) : Parcelable
+data class TestData(val number: Int) {
+    companion object {
+        val saver = Saver<MutableState<TestData>, Int>(
+            save = {
+                it.value.number
+            },
+            restore = {
+                mutableStateOf(TestData(it))
+            }
+        )
+    }
+}
